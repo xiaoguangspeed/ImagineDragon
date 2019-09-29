c757a03d274425b89f3a3a6daf645031e153eea3
xiaoguangspeed <xiaoguangspeed@126.com>
2019-09-29 23:11:56 +0800

    9-29

---
+++ Code/JS/es6.js
@@ -0,0 +1,1 @@
+// import React {suspence, lazy} from react;
--- Code/JS/test.js
+++ Code/JS/test.js
@@ -1,5 +1,12 @@
-let assert = require('assert')
+let net = require('net')
+const server = net.createServer((socket) => {
+  socket.end('再见\n');
+}).on('error', (err) => {
+  // 处理错误
+  throw err;
+});
 
-let fn = 123
-
-assert('function' == typeof fn, 'function required')
+// 获取任意未使用的端口。
+server.listen(() => {
+  console.log('打开服务器', server.address());
+});
